5cfea94795ec0460dc236d7b9160a42892892fd2
Vagapova Valeriia <lera.vagapova@gmail.com>
2023-02-17 11:16:47 +0200

    edit register component

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate, Link } from "react-router-dom";
+
+import { Button } from "react-bootstrap";
+
+import { auth, registerWithEmailAndPassword } from "../auth/firebase";
 
 const Register = () => {
-  return <div>Register here</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  // This hook will store the user for us
+  const [user, loading, error] = useAuthState(auth);
+  const navigate = useNavigate();
+
+  const register = () => {
+    if (!name) alert("Please enter name");
+    registerWithEmailAndPassword(name, email, password);
+  };
+
+  useEffect(() => {
+    if (loading) return;
+    // If there is a user, we just navigate to home page
+    if (user) navigate("/countries");
+  }, [user, loading, navigate]);
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Full name"
+      />
+      <input
+        type="text"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      <Button onClick={register}>Register</Button>
+      <div>
+        Acready have an account?
+        <Link to="/login">Login</Link> now.
+      </div>
+    </div>
+  );
 };
 
 export default Register;
