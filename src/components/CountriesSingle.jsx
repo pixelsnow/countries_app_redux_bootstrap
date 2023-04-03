09424f90091057225db22df5e0a521c7b56622a6
Vagapova Valeriia <lera.vagapova@gmail.com>
2023-04-03 20:06:39 +0300

    edit gitignore

--- src/components/CountriesSingle.jsx
+++ src/components/CountriesSingle.jsx
@@ -19,24 +19,25 @@ const CountriesSingle = () => {
   const [country, setCountry] = useState(undefined);
 
   useEffect(() => {
-    /*  if (location.state.country) { */
-    console.log("state exists");
-    setCountry(location.state.country);
-    //setLoading(false);
-    axios
-      .get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${location.state.country.capital}&units=metric&appid=${process.env.REACT_APP_OPENWEATHER_KEY}`
-      )
-      .then((res) => {
-        console.log("weather data", res.data);
-        setWeather(res.data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        console.log(err);
-        setError(true);
-      });
-    /* } else {
+    if (location.state.country) {
+      /*  if (location.state.country) { */
+      console.log("state exists");
+      setCountry(location.state.country);
+      //setLoading(false);
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${location.state.country.capital}&units=metric&appid=${process.env.REACT_APP_OPENWEATHER_KEY}`
+        )
+        .then((res) => {
+          console.log("weather data", res.data);
+          setWeather(res.data);
+          setLoading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+          setError(true);
+        });
+    } else {
       console.log("no state");
       countryService.getSingle(single).then((res) => {
         country = res.data;
@@ -55,7 +56,7 @@ const CountriesSingle = () => {
             setError(true);
           });
       });
-    } */
+    }
   }, [country]);
 
   useEffect(() => console.log("loading", loading), [loading]);
