0ddc241016a17002cfc64bc38c44b87636d9a37c
Vagapova Valeriia <lera.vagapova@gmail.com>
2023-04-08 23:43:29 +0300

    install sass

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import App from "./App";
 import store from "./app/store";
 import "./index.css";
+import "./styles/stylesheet.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
