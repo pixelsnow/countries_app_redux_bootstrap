94dc7356eab13b42684a1d820c88073cc70aba68
Vagapova Valeriia <lera.vagapova@gmail.com>
2023-04-12 14:53:39 +0300

    style home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Button, Row } from "react-bootstrap";
+import { Button, Row, Col } from "react-bootstrap";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { getAuth } from "firebase/auth";
 
 const Home = () => {
   const [flags, setFlags] = useState([]);
@@ -19,20 +21,11 @@ const Home = () => {
     console.log("num of flags", flags.length);
   }, [flags]);
 
+  const auth = getAuth();
+  const [user] = useAuthState(auth);
+
   return (
-    <>
-      <div className="home-container">
-        <div className="home-content-container">
-          <div className="home-content">
-            <span>Countries app </span>is a simple React application made in
-            Business College Helsinki lessons. App uses{" "}
-            <a href="https://restcountries.com/">REST Countries API </a>,{" "}
-            <a href="https://openweathermap.org/">Open weather API</a>
-            <Button variant="custom">Log in</Button>
-            <Button variant="custom">Sign up</Button>
-          </div>
-        </div>
-      </div>
+    <Col>
       <Row>
         <div className="slider">
           <div className="all-flags">
@@ -49,7 +42,51 @@ const Home = () => {
           </div>
         </div>
       </Row>
-    </>
+      <Row className="home-container">
+        <Col className="home-content">
+          <h1>Welcome to countries app</h1>
+          <p>
+            This is a simple React application that showcases all countries in
+            the world
+          </p>
+          {!user && (
+            <div className="call-to-action">
+              <h3>Please login or sign up to browse countries</h3>
+              <div className="buttons">
+                <Button variant="custom">Log in</Button>
+                <Button variant="custom">Sign up</Button>
+              </div>
+            </div>
+          )}
+          <h2>Technologies used</h2>
+          <ul>
+            <li>React</li>
+            <li>Redux, RTK</li>
+            <li>Firebase (authentication, data storage)</li>
+            <li>Axios</li>
+          </ul>
+          <h2>Resources used</h2>
+          <ul>
+            <li>
+              <a href="https://restcountries.com/">REST Countries API </a>
+            </li>
+            <li>
+              <a href="https://openweathermap.org/">Open weather API</a>
+            </li>
+            <li>
+              <a href="https://developers.google.com/maps/documentation/javascript">
+                Google Maps Javascript API
+              </a>
+            </li>
+            <li>
+              <a href="https://developers.google.com/maps/documentation/javascript/reference/places-service">
+                Google Places Service
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Col>
   );
 };
 
